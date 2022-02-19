191a473fe80b2565869ed0051aa726c9c73bfa1c
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-19 12:41:55 +0100

    :test_tube: update test

--- BowlingGameKataDotNet/BowlingGameKata.Tests/BowlingGameFeature.cs
+++ BowlingGameKataDotNet/BowlingGameKata.Tests/BowlingGameFeature.cs
@@ -9,6 +9,26 @@ namespace BowlingGameKata.Tests
         public void score_a_game_without_any_bonus()
         {
             var game = new BowlingGame();
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(9);
+            game.Roll(0);
+            game.Roll(9);
+            game.Roll(0);
 
             var score = game.Score();
 
