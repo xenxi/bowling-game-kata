0995c90a8dabdf0c677d738231e2644ab3729613
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-19 14:21:06 +0100

    :white_check_mark: fix tests

--- BowlingGameKataDotNet/BowlingGameKata/BowlingGame.cs
+++ BowlingGameKataDotNet/BowlingGameKata/BowlingGame.cs
@@ -3,16 +3,14 @@
     public class BowlingGame
     {
         private Frame _current;
-        private Frame _first;
         private int _frame = 0;
-        private Frame _second;
+        private IList<Frame> _frames;
         private int _tries = 0;
 
         public BowlingGame()
         {
-            _first = new Frame();
-            _second = new Frame();
-            _current = _first;
+            _current = new Frame();
+            _frames = new List<Frame> { _current };
 
             _frame = 0;
             _tries = 0;
@@ -28,13 +26,17 @@
             _frame = _tries / 2;
 
             if (_current.Completed())
-                _current = _second;
+            {
+                _current = new Frame();
+                _frames.Add(_current);
+            }
         }
 
         public int Score()
         {
-            var score = _first.Score + _second.Score;
-            var bunuses = IsStrike() ? _second.Score : 0;
+            var score = _frames.Sum(f => f.Score);
+
+            var bunuses = IsStrike() ? _frames.ElementAt(1).Score : 0;
 
             return score + bunuses;
         }
@@ -45,6 +47,6 @@
                 throw new CompletedGame();
         }
 
-        private bool IsStrike() => _first.Score == 10;
+        private bool IsStrike() => _frames.First().Score == 10;
     }
 }
