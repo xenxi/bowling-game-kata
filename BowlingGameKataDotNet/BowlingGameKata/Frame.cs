d3bb513d683e53911aad3df65e4a94c9d0de80e6
xenxi <antoniom.diaz.moreno@gmail.com>
2022-02-20 13:50:53 +0100

    :recycle: refact remove property

--- BowlingGameKataDotNet/BowlingGameKata/Frame.cs
+++ BowlingGameKataDotNet/BowlingGameKata/Frame.cs
@@ -5,7 +5,6 @@
         private int _first = 0;
         private int _tries = 0;
         public int Score { get; private set; } = 0;
-        public int Tries => _tries;
 
         public void Anotate(int pinsDown)
         {
@@ -22,11 +21,12 @@
 
             _tries++;
         }
-        public bool IsStrike() => HasBonus() && _tries == 1;
 
-        public bool HasBonus() => Score == 10;
         public bool Completed() => _tries > 1 || Score == 10;
 
+        public bool HasBonus() => Score == 10;
+
+        public bool IsStrike() => HasBonus() && _tries == 1;
         public int SpareBonus() => _first;
     }
 }
